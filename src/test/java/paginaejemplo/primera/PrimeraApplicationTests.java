915ffc0d3443a8a16174c579dda7665ad581fdfd
dan1l023 <pablodanilotoreesjimenez@gmail.com>
2023-10-30 10:12:12 -0500

    agregue  .gitignore         new file:   build.gradle         new file:   gradle/wrapper/gradle-wrapper.jar         new file:   gradle/wrapper/gradle-wrapper.properties         new file:   gradlew         new file:   gradlew.bat         new file:   settings.gradle         new file:   src/main/java/paginaejemplo/primera/PrimeraApplication.java         new file:   src/main/resources/application.properties         new file:   src/test/java/paginaejemplo/primera/PrimeraApplicationTests.java

---
+++ src/test/java/paginaejemplo/primera/PrimeraApplicationTests.java
@@ -0,0 +1,13 @@
+package paginaejemplo.primera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
